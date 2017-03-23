70392eda5296c52e6e72d29afc960012f91e8992
Caleb Lee <caleb.gwl@gmail.com>
2017-03-23 15:14:44 -0500

    Manufacturer new form

--- app/manufacturers-new/controller.js
+++ app/manufacturers-new/controller.js
@@ -1,5 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  formValues: {},
+  formValues: {
+    name: '',
+    country: '',
+  },
+
+  actions: {
+    submitForm() {
+      const manufacturer = this.store.createRecord('manufacturer', this.formValues);
+
+      manufacturer.save().then((manufacturer) => {
+        this.set('formValues', {});
+
+        this.transitionToRoute('manufacturers', manufacturer.id);
+      });
+    }
+  }
+
 });
--- app/manufacturers/route.js
+++ app/manufacturers/route.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return fetch('https://razerapi.herokuapp.com/ryan/basic/brands')
+    return fetch('https://razerapi.herokuapp.com/caleb/basic/brands')
     .then(r => r.json());
   }
 });
